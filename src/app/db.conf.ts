0c2c7f8b1f6be41ba59387eac25b2cea011c7374
Chloe <44chloeguilbaud@gmail.com>
2018-10-24 15:38:29 +0200

    docs: Database configuration documentation

--- src/app/db.conf.ts
+++ src/app/db.conf.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
+/**
+ * Database connfiguration information
+ */
 export class DbConf {
 
   public static databasename: string = "devfest.db";
