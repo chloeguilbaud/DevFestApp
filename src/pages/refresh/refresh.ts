9a1da249b116a36a3b490bd335a15f4d47b04185
Chloe <44chloeguilbaud@gmail.com>
2018-10-26 17:18:26 +0200

    feat: Refresh cache - alert if no internet

--- src/pages/refresh/refresh.ts
+++ src/pages/refresh/refresh.ts
@@ -3,6 +3,7 @@ import { Device } from '@ionic-native/device';
 import { Network } from '@ionic-native/network';
 import { File } from '@ionic-native/file';
 import { IonicApp, NavController } from 'ionic-angular';
+import {ErrorAlertHandler} from "../../manager/error.handler/error.alert.handler";
 
 @Component({
   selector: 'page-refresh',
@@ -10,18 +11,23 @@ import { IonicApp, NavController } from 'ionic-angular';
 })
 export class RefreshPage {
 
-  constructor(private network: Network) {
+  constructor(private network: Network, private alertHandler: ErrorAlertHandler) {
+
     // Verify if internet is enabled
     if (network.type == "none") {
-      console.log("pas d'internet");
+      console.log("Internet disabled");
+      alertHandler.presentAlert("Network", "Y a pas internet... C'est pas une bonne idée de tout vider...", "Tu as raison.");
     } else {
-      console.log("internet dispo");
+      console.log("Internet enabled");
+
+      // Empty cache
       if (navigator.serviceWorker.controller) {
 
         navigator.serviceWorker.controller.postMessage({
             "command": "CLEAR_CACHE",
             "message": "CLEAR_CACHE"
         });
+
       }
     }
 
