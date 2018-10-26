1a309423e30695c21f437eee954bee1c70acf502
Chloe <44chloeguilbaud@gmail.com>
2018-10-26 08:37:51 +0200

    merge

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -5,8 +5,8 @@ import { File } from '@ionic-native/file';
 import { ImagePicker, ImagePickerOptions } from "@ionic-native/image-picker";
 
 import { ErrorAlertHandler } from "../../manager/error.handler/error.alert.handler";
-import {Session} from "../../entities/session";
-import {DbManager} from "../../database/db.manager";
+import { Session } from "../../entities/session";
+import { DbManager } from "../../database/db.manager";
 
 @Component({
   selector: 'page-notes',
@@ -26,6 +26,7 @@ export class NotesPage {
 
     this.s = new Session(450, "", "");
 
+    //this.dbManager.saveSessionImage(450, );
     this.loadNotes();
     this.loadImage();
 
@@ -39,7 +40,7 @@ export class NotesPage {
         this.note_txt = res;
       }
     ).catch((err) => {
-      this.noteEditErrorHandler(err);
+      this.handleNoteError(err);
     });
   }
 
@@ -47,7 +48,11 @@ export class NotesPage {
    * Loading the image of the current session (if there is one)
    */
   public loadImage() {
-
+    this.dbManager.getSessionImage(this.s.id).then((res) => {
+      this.image_src = res;
+    }).catch((err) => {
+      this.handleImageError(err);
+    });
   }
 
   /**
@@ -56,7 +61,7 @@ export class NotesPage {
   saveNotes() {
     this.dbManager.saveSessionNote(this.s.id, this.note_txt)
       .catch((err) => {
-        this.noteEditErrorHandler(err);
+        this.handleNoteError(err);
       }
     );
   }
@@ -79,7 +84,7 @@ export class NotesPage {
     };
 
     this.camera.getPicture(options).then((imageData) => {
-      this.parseImg(imageData);
+      this.parseAndSaveImg(imageData);
     }, (err) => {
       console.error(err);
       this.alertHandler.presentAlert("Oups...", "Prise de photo annulée... retente plus tard?", "Ok :'(");
@@ -98,7 +103,8 @@ export class NotesPage {
 
     this.imgPicker.getPictures(options).then((results) => {
       console.log("res", results);
-      this.parseImg(results[0]);
+      this.parseAndSaveImg(results[0]);
+
     }, (err) => {
       console.error(err);
       this.alertHandler.presentAlert("Oups...", "Sélection de photo annulée... retente plus tard?", "Ok :'(");
@@ -110,26 +116,39 @@ export class NotesPage {
    * Parse image to be displayed on page
    * @param {string} imageData image data
    */
-  private parseImg(imageData: string) {
+  private parseAndSaveImg(imageData: string) {
     // Spliting the file and the path from FILE_URI result
     console.log("imageData", imageData);
     let filename = imageData.substring(imageData.lastIndexOf('/')+1);
     console.log("filename", filename);
     let path =  imageData.substring(0,imageData.lastIndexOf('/')+1);
     console.log("path", path);
-    // Using the method readDataURL
+    // Transforming and saving image data
     this.file.readAsDataURL(path, filename).then(res => {
       this.image_src = res;
-    } );
+      console.log("parsedimage", res);
+      this.dbManager.saveSessionImage(this.s.id, res).catch((err) => {
+        this.handleImageError(err);
+      });
+    });
   }
 
   /**
    * Note error handler
    * @param err error
    */
-  private noteEditErrorHandler(err: any) {
+  private handleNoteError(err: any) {
     console.error(err);
     this.alertHandler.presentAlert("Humm...", "Tu n'as pas encore de note... ou alors tes notes ont été supprimées...", "Je te pardonne");
+  }
+
+  /**
+   * Image error handler
+   * @param err error
+   */
+  private handleImageError(err: any) {
+    console.error(err);
+    this.alertHandler.presentAlert("Humm...", "Tu n'as pas encore pris de photo... ou alors tes notes ont été supprimées...", "Je te pardonne");
   }
 
 }
